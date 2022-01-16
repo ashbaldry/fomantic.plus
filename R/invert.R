96d2fa19ba4f28fe5b1b19c3ab99af9601dc4cdf
Ashley Baldry <arbaldry91@gmail.com>
2022-01-16 17:01:04 +0000

    Adding invert toggle

---
+++ R/invert.R
@@ -0,0 +1,63 @@
+#' Invert Toggle
+#'
+#' @description
+#' Add a toggle to the shiny application that triggers all Fomantic UI elements to become "inverted"
+#'
+#' @param pre_label,post_label Labels to add before and after the toggle. By default "Dark Mode" will appear after
+#' the toggle
+#' @param ... Tag attributes (named arguments) and children (unnamed arguments)
+#' @param checked Should the application start off in dark mode?
+#'
+#' @examples
+#' if (interactive()) {
+#'   library(shiny)
+#'   library(shiny.semantic)
+#'   ui <- semanticPage(
+#'     extendShinySemantic(),
+#'     fui_el$grid(
+#'       fui_el$row(
+#'         class = "two column",
+#'         fui_el$column(
+#'           fui_el$segment(
+#'             class = "purple",
+#'             darkmode_toggle()
+#'           )
+#'         ),
+#'         fui_el$column(
+#'           fui_el$segment(
+#'             class = "red"
+#'           )
+#'         )
+#'       )
+#'     ),
+#'     fui_el$cards(
+#'       class = "two",
+#'       fui_el$card(),
+#'       fui_el$card()
+#'     )
+#'   )
+#'
+#'   server <- function(input, output, session) {}
+#'
+#'   shiny::shinyApp(ui, server)
+#' }
+#'
+#' @export
+darkmode_toggle <- function(label = "Dark Mode", ..., checked = FALSE) {
+  if (isTRUE(checked)) {
+    checked_attr <- "checked"
+  } else {
+    checked_attr <- NULL
+  }
+
+  fui_el$form(
+    fui_el$field(
+      div(
+        class = "ui toggle checkbox invert-toggle",
+        tags$input(type = "checkbox", checked = checked_attr),
+        tags$label(label),
+        ...
+      )
+    )
+  )
+}
--- inst/srcjs/fomantic-plus/fomantic-plus.js
+++ inst/srcjs/fomantic-plus/fomantic-plus.js
@@ -3,6 +3,22 @@ $(document).ready(function() {
     $('.navbar-collapsible-item').transition('fade down');
   });
 
+  $('.invert-toggle').checkbox({
+    onChecked: () => {
+      var fui_elements = document.getElementsByClassName('ui');
+      $(fui_elements).addClass('inverted');
+    },
+    onUnchecked: () => {
+      var fui_elements = document.getElementsByClassName('ui');
+      $(fui_elements).removeClass('inverted');
+    }
+  });
+
+  if ($('.invert-toggle').checkbox('is checked')) {
+    var fui_elements = document.getElementsByClassName('ui');
+    $(fui_elements).addClass('inverted');
+  }
+
   if (window.innerWidth < 768) {
     $('.navbar-collapsible-item').transition('hide');
   }
